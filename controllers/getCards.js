12169fb753daac44a61f6008aec5e1bad867892d
Aziik <arslanfazilov93@gmail.com>
2020-10-22 16:39:07 +0300

    исправлены недочеты

--- controllers/getCards.js
+++ controllers/getCards.js
@@ -4,6 +4,6 @@ const jsonDataPath = path.join(__dirname, '..', 'data', 'cards.json')
 const getCards = (req, res) => {
   readFile(jsonDataPath)
   .then(data => res.send(data))
-  .catch(() => res.status(500).send({ "mesage": "Файл с данными не найден" })
+  .catch(() => res.status(500).send({ "mesage": "Файл с данными не найден" }))
 }
 module.exports = { getCards }
--- routes/card.js
+++ routes/card.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 const { getCards } = require('../controllers/getCards');
-router.get('/cards', getCards());
+router.get('/cards', getCards);
 
 module.exports = router
