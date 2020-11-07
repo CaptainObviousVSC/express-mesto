1646af9072fc4d604a744821657bed637db656cf
Aziik <arslanfazilov93@gmail.com>
2020-11-07 13:32:04 +0300

    тест

--- routes/cards.js
+++ routes/cards.js
@@ -4,4 +4,4 @@ const { getCards, postCards, deleteCard } = require('../controllers/getCards');
 router.get('/cards', getCards);
 router.delete('/cards/:cardId', deleteCard);
 router.post('/cards', postCards);
-module.exports = { router };
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,4 @@ const { getUsers, getUser, postUsers } = require('../controllers/getUsers');
 router.get('/users', getUsers);
 router.get('/users/:_id', getUser);
 router.post('/users', postUsers);
-module.exports = { router };
+module.exports = router;
